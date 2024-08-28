e7b7f9ceb43229a369e08edcdf0332813dd680e5
Walber Zaldivar Herrera <walber.zaldivar@gmail.com>
2024-08-28 00:30:41 -0300

    test update

--- src/components/ownership-label/hooks/use-current-year.test.ts
+++ src/components/ownership-label/hooks/use-current-year.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it, jest } from '@jest/globals';
-import { renderHook } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 
 import { advanceYear, setupTimers } from './timers-test-utils';
 import useCurrentYear, {
@@ -11,13 +11,14 @@ import useCurrentYear, {
 describe('useCurrentYear', () => {
   const initialYear = 2020;
   const endYear = 2021;
+
   setupTimers(initialYear);
 
   it('getTimerInterval should decrease to 1 second', () => {
     let timerInterval = getTimerInterval();
 
     while (timerInterval != MILLISECONDS_IN_SECOND) {
-      jest.advanceTimersByTime(timerInterval);
+      act(() => jest.advanceTimersByTime(timerInterval));
       const newTimerInterval = getTimerInterval();
 
       expect(newTimerInterval).toBeLessThanOrEqual(timerInterval);
